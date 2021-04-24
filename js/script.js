28247e3cf8992243e1d587a38f0d5848a89c0611
Ajish <ajishtmohan@gmail.com>
2021-04-24 16:32:13 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,64 @@
+setTimeout(function () {
+  document.querySelector(".openinglogo").style.width = "18%";
+  document.querySelector(".openinglogo").style.opacity = 0;
+  setTimeout(function () {
+    document.querySelector(".opening").style.opacity = 0;
+    setTimeout(function () {
+      document.querySelector(".opening").style.display = "none";
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".uheader").style.top = "0px";
+    document.querySelector(".uheader").style.height = "50px";
+    document.querySelector(".uheader").style.zIndex = "15";
+
+    document.querySelector(".lheader").style.top = "50px";
+    document.querySelector(".lheader").style.height = "100px";
+
+    document.querySelector(".menus").style.height = "100px";
+
+    document.querySelector(".singleline").style.opacity = "0";
+    setTimeout(function () {
+      document.querySelector(".singleline").style.display = "none";
+      document.querySelector(".doubleline").style.display = "block";
+    }, 300);
+    setTimeout(function () {
+      document.querySelector(".doubleline").style.opacity = "1";
+    }, 500);
+
+    var links = document.querySelectorAll(".links");
+    for (var i = 0; i < links.length; i++) {
+      links[i].style.margin = "32px 0px 0px";
+    }
+  } else {
+    document.querySelector(".uheader").style.top = "-51px";
+    // document.querySelector(".uheader").style.height = "0px";
+
+    document.querySelector(".lheader").style.height = "70px";
+    document.querySelector(".lheader").style.top = "0px";
+
+    document.querySelector(".lheader").style.top = "0px";
+
+    document.querySelector(".menus").style.height = "50px";
+
+    document.querySelector(".doubleline").style.opacity = "0";
+    setTimeout(function () {
+      document.querySelector(".doubleline").style.display = "none";
+      document.querySelector(".singleline").style.display = "block";
+    }, 300);
+    setTimeout(function () {
+      document.querySelector(".singleline").style.opacity = "1";
+    }, 500);
+
+    var links = document.querySelectorAll(".links");
+    for (var i = 0; i < links.length; i++) {
+      links[i].style.margin = "17px 0px 0px";
+    }
+  }
+  prevScrollpos = currentScrollPos;
+};
